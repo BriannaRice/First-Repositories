7ee38af2a8de40308fcb7342ce01892a8aa0f37e
Brianna Rice <brianna.rice@gulllakecs.org>
2019-02-11 13:01:43 -0500

    Add the functions and variables

--- Function - Git.py
+++ Function - Git.py
@@ -1,11 +1,27 @@
 # 4.13.3:Greetings
 # Brianna Rice
 # 2.5.19
-
+'''
 name = input("What is your name: ")
 
 def greeting():
     print("Hi there " + name + "!")
     print("Nice to meet you ")
 
-greeting() 
+greeting()
+'''
+
+# 4.13.: Functions and variables
+# Brianna Rice
+# 2.11.19
+
+x = 406
+
+def print_something():
+    x = 3
+    print('\r\n',x)
+
+print('\r\n', x)
+print_something()
+
+
